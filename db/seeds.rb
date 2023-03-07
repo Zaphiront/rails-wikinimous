4cd6d7955beceac9cce5ec60b388223754a20570
Paul Huallpa <pantoni123@gmail.com>
2023-03-07 15:03:53 -0600

    3rd ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  article = Article.new(
+    title:    Faker::Games::SuperSmashBros.fighter,
+    content: Faker::Games::SuperSmashBros.stage
+  )
+  article.save!
+end
